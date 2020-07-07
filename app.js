9d14fcd09c96cf55615dbd3e5a7dd2c09f0b35b4
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2020-07-07 14:58:53 +0800

    Multiple route added
    Multiple route is added based on the pages.
    Minor code change on app.js

--- app.js
+++ app.js
@@ -8,11 +8,21 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static("public"))
+app.use(express.static(__dirname + "/public"))
 
-app.get("/", function(req, res) {
+app.get("/aduan", function(req, res) {
+  res.render("log-masuk")
+})
+
+app.get("/aduan/buat-aduan", function(req, res) {
+  res.render("buat-aduan")
+})
+
+app.get("/aduan/semakan-aduan", function(req, res) {
   res.render("semakan-aduan")
 })
+
+
 app.listen(3000, function() {
   console.log("Server started on port 3000")
 })
