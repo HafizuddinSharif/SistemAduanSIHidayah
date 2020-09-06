daea6b7e73c9b2d94eedb4db353003347b3fa70e
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2020-09-06 08:20:10 +0100

    Added Procfile

--- app.js
+++ app.js
@@ -745,6 +745,12 @@ function checkNotAuthenticated(req, res, next) {
   next()
 }
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server started on port 3000")
 })
